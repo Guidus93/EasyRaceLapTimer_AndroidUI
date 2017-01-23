62ef3552dc2e86a247a0f24a4b6e39379ee493c0
emiltom95 <emiliano.tomaro@gmail.com>
2017-01-23 14:17:15 +0100

    Trying to committing something

--- app/src/main/java/com/example/guidofabbrini/easyracelaptimerui/MainActivity.java
+++ app/src/main/java/com/example/guidofabbrini/easyracelaptimerui/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    //onCreated finished
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
